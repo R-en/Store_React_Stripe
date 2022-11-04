0a488c6a3abd610b031b56c88c29b4b68bea515d
Ren <renli@uw.edu>
2022-11-04 16:31:34 -0700

    Added modal,card, and context prvider

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComponent from "./Components/Navbar";
 import { Container } from "react-bootstrap";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartProvider from "./CardContext";
 import Store from "./Pages/Store";
 import Success from "./Pages/Success";
 import Cancel from "./Pages/Cancel";
 
 function App() {
   return (
-    <Container>
-      <NavbarComponent></NavbarComponent>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Store />}></Route>
-          <Route path="success" element={<Success />}></Route>
-          <Route path="cancel" element={<Cancel />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </Container>
+    <CartProvider>
+      <Container>
+        <NavbarComponent></NavbarComponent>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Store />}></Route>
+            <Route path="success" element={<Success />}></Route>
+            <Route path="cancel" element={<Cancel />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </CartProvider>
   );
 }
 
--- src/Pages/Store.js
+++ src/Pages/Store.js
@@ -1,15 +1,18 @@
 import { Row, Col } from "react-bootstrap";
+import { productsArray } from "../productsStore";
+import ProductCard from "../Components/productCard";
 function Store() {
   return (
     <>
-      <h1>Welcome to the Store</h1>
+      <h1 align="center" className="p-3">
+        Welcome to the Store
+      </h1>
       <Row xs={1} md={3} className="g-4">
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
-        <Col align="center">
-          <h1>Product</h1>
-        </Col>
+        {productsArray.map((product, idx) => (
+          <Col align="center" key={idx}>
+            <ProductCard product={product} />
+          </Col>
+        ))}
       </Row>
     </>
   );
